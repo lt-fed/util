f06612820bed77c4f06393c9c6f6d7974f45c581
renaesop <njurjs@gmail.com>
2016-03-04 17:38:44 +0800

    增加一些is的单元测试

--- test/isSpec.js
+++ test/isSpec.js
@@ -1,85 +1,103 @@
 /**
  * Created by Yuji on 2016/3/4.
  */
-define(['is'], function(is){
-  describe('Module IS', function(){
-    describe('typeList', function(){
-      describe('#isArray', function(){
-        it('should return true when given an array', function(){
+define(['is'], function (is) {
+  describe('Module IS', function () {
+    describe('typeList', function () {
+      describe('#isArray', function () {
+        it('should return true when given an array', function () {
           expect(is.isArray([])).toEqual(true);
         });
       });
-      describe('#isNaN', function(){
-        it('should return false when not given a NaN', function(){
+      describe('#isNaN', function () {
+        it('should return false when not given a NaN', function () {
           expect(is.isNaN(1)).toEqual(false);
         });
-        it('should return true when given a NaN', function(){
+        it('should return true when given a NaN', function () {
           expect(is.isNaN(NaN)).toEqual(true);
         });
 
       });
-      describe('#isNumber', function(){
-        it('should return false when not given a Number', function(){
+      describe('#isNumber', function () {
+        it('should return false when not given a Number', function () {
           expect(is.isNumber(NaN)).toEqual(false);
           expect(is.isNumber('abc')).toEqual(false);
         });
-        it('should return true when given a NaN', function(){
+        it('should return true when given a NaN', function () {
           expect(is.isNumber(1234431)).toEqual(true);
         });
+      });
+      describe('#isObject', function () {
+        it('should return true if it\'s obj', function(){
+          expect(is.isObject({})).toEqual(true);
+          typeof Object.create ==='function'&&expect(is.isObject(Object.create(null))).toEqual(true);
+        });
+        it('should return false if given an array', function(){
+          expect(is.isObject([])).toEqual(false);
+        });
+      });
+      describe('#isFunction', function () {
+
+      });
+      describe('#isString', function () {
+
+      });
+      describe('#isNull', function () {
+
       });
     });
 
 
-    describe('inputValidator', function(){
-      
-        var pattern = '  should return whether it is';
-        it('#email'+ pattern , function(){
-          expect(is.email('qaq@luo.bo')).toEqual(true);
-          expect(is.email('qqq@a.b')).toEqual(false);
-          expect(is.email('测试')).toEqual(false);
-        });
-        it('#phoneNumber' + pattern, function(){
+    describe('inputValidator', function () {
 
-        });
-        it('#zipcode' +pattern, function(){
+      var pattern = '  should return whether it is';
+      it('#email' + pattern, function () {
+        expect(is.email('qaq@luo.bo')).toEqual(true);
+        expect(is.email('qqq@a.b')).toEqual(false);
+        expect(is.email('测试')).toEqual(false);
+      });
+      it('#phoneNumber' + pattern, function () {
 
-        });
-        it('#password'+pattern, function(){
+      });
+      it('#zipcode' + pattern, function () {
 
-        });
-        it('#name' + pattern, function(){
+      });
+      it('#password' + pattern, function () {
 
-        });
-        it('#nickName' + pattern, function(){
+      });
+      it('#name' + pattern, function () {
 
-        });
-        it('#city' +pattern, function(){
+      });
+      it('#nickName' + pattern, function () {
 
-        });
-        it('#address' +pattern, function(){
+      });
+      it('#city' + pattern, function () {
 
-        });
-        it('#couponCode' +pattern, function(){
+      });
+      it('#address' + pattern, function () {
 
-        });
-        it('#cardNumber' +pattern, function(){
+      });
+      it('#couponCode' + pattern, function () {
 
-        });
-        it('#secureCode'+pattern, function(){
+      });
+      it('#cardNumber' + pattern, function () {
 
-        });
-        it('#quantity'+pattern, function(){
-          expect(is.quantity(0.1)).toEqual(false);
-          expect(is.quantity(123)).toEqual(true);
-          expect(is.quantity('0.1')).toEqual(false);
-          expect(is.quantity('123')).toEqual(true);
-        });
-        it('#price'+pattern, function(){
-          expect(is.price(3.22)).toEqual(true);
-          expect(is.price('3.22')).toEqual(true);
-          expect(is.price(3.113)).toEqual(false);
-          expect(is.price('$$')).toEqual(false);
-        });
+      });
+      it('#secureCode' + pattern, function () {
+
+      });
+      it('#quantity' + pattern, function () {
+        expect(is.quantity(0.1)).toEqual(false);
+        expect(is.quantity(123)).toEqual(true);
+        expect(is.quantity('0.1')).toEqual(false);
+        expect(is.quantity('123')).toEqual(true);
+      });
+      it('#price' + pattern, function () {
+        expect(is.price(3.22)).toEqual(true);
+        expect(is.price('3.22')).toEqual(true);
+        expect(is.price(3.113)).toEqual(false);
+        expect(is.price('$$')).toEqual(false);
+      });
     })
   })
 });
