3fdccd88438445edc91e068d1404cc2f613ff423
renaesop <njurjs@gmail.com>
2016-03-05 12:57:13 +0800

    调整scripts

--- karma.conf.js
+++ karma.conf.js
@@ -58,7 +58,7 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['PhantomJS', 'Chrome', 'IE', 'Firefox'],
+    browsers: ['PhantomJS', 'Chrome', 'IE', 'Firefox', 'Safari'],
 
 
     // Continuous Integration mode
